e6f054329e3a5ab2f2e00ac5b18699ef29545405
DerekHJH <41600057+DerekHJH@users.noreply.github.com>
2021-07-03 11:25:33 +0800

    run IntelliJ IDEA

---
+++ codes/src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld
+{
+    public static void main(String[] args)//type psvm and tab
+    {
+        System.out.println("Hello World!");//type sout and tab
+    }
+}
+//click the top left green arrow to run the method
